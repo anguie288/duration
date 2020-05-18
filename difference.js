e7aa0f81bdc869511227b3e99e259bc4214cd8b9
anguie288 <61396665+anguie288@users.noreply.github.com>
2020-05-18 17:34:13 -0400

    Update difference.js

--- difference.js
+++ difference.js
@@ -5,12 +5,14 @@ function diff_minutes(d1, d2) {
     return Math.abs(diffInHours);
   }
 
-    d01= new Date("May 18, 2020 09:11:00"); //d1 = new Date(2020,05,18);
-    d1 = d01.getTime();
-    d11 = d01.getHours(); d11m= d01.getMinutes();
+    d01= new Date("May 18, 2020 17:30:00");   d1 = d01.getTime();//d1 = new Date(2020,05,18);
+    d11 = d01.getHours();  d11m= d01.getMinutes();
+    // Military Add 2 hrs
+    
+    d02= new Date("May 18, 2020 19:45:00");   d2= d02.getTime();//d2 = new Date(2020,05,19);
+    d22= d02.getHours();   d22m= d02.getMinutes();
 
-    d02= new Date("May 20, 2020 16:11:00"); //d2 = new Date(2020,05,19);
-    d2= d02.getTime();
-    d22= d02.getHours(); d22m= d02.getMinutes();
+//console.log( "From ("+d11+":"+d11m+") "+"to ("+d22+":"+d22m+") "+"Total ("+diff_minutes(d1, d2)+")");
+// afternoon
+console.log( "From ("+(d11-12)+":"+d11m+") "+"to ("+(d22-12)+":"+d22m+") "+"Total ("+diff_minutes(d1, d2)+")");
 
-console.log( "From ("+d11+":"+d11m+") "+"to ("+d22+":"+d22m+") "+"Total ("+diff_minutes(d1, d2)+")");
