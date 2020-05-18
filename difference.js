9395e4386df9d4cd694c757dd8f5ad67281284d0
anguie288 <61396665+anguie288@users.noreply.github.com>
2020-05-18 17:36:43 -0400

    Update difference.js

--- difference.js
+++ difference.js
@@ -7,7 +7,7 @@ function diff_minutes(d1, d2) {
 
     d01= new Date("May 18, 2020 17:30:00");   d1 = d01.getTime();//d1 = new Date(2020,05,18);
     d11 = d01.getHours();  d11m= d01.getMinutes();
-    // Military Add 2 hrs
+    // Military Add 2 hrs  5-17
     
     d02= new Date("May 18, 2020 19:45:00");   d2= d02.getTime();//d2 = new Date(2020,05,19);
     d22= d02.getHours();   d22m= d02.getMinutes();
